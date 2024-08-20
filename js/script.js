b00ebba2a1dce374c3b7324adcbc59a56ff65d4b
Richard Reis <167144386+richxrdreis@users.noreply.github.com>
2024-08-20 18:45:12 -0300

    criando o efeito de rolagem com js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    const homeSection = document.querySelector('#home');
+    const homeSectionHeight = homeSection.offsetHeight;
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > homeSectionHeight) {
+            header.classList.add('header-scrolled');
+        } else {
+            header.classList.remove('header-scrolled');
+        }
+    });
+});
+
